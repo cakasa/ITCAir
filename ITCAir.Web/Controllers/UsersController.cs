12a250f679c53f5d702f6bc057397103e4ff2aa8
cakas <cakas@abv.bg>
2020-02-15 18:17:31 +0200

    Structure

---
+++ ITCAir.Web/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITCAir.Data;
+using ITCAir.Web.Models;
+using ITCAir.Web.Models.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITCAir.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ITCAirContext context;
+
+        public UsersController()
+        {
+            context = new ITCAirContext();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            ViewData["Message"] = "";
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(UserSignInViewModel user)
+        {
+            if (context.Users.Any(x => x.Username == user.Username && x.Password == user.Password))
+            {
+                // Do something
+                return View("Index");
+            }
+            else
+            {
+                ViewData["Message"] = "Password or username is incorrect!";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserSignUpViewModel user)
+        {
+            return View("SignIn");
+        }
+    }
+}
--- ITCAir.Web/Startup.cs
+++ ITCAir.Web/Startup.cs
@@ -50,7 +50,7 @@ namespace ITCAir.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Reservations}/{action=Index}/{id?}");
             });
         }
     }

34ff24c4f17c6c70d9ce16eab9f967ee0b190b63
gotinpet22@gmail.com <gotinpet22@gmail.com>
2020-02-16 11:26:43 +0200

    Reservation Page Half Done

--- ITCAir.Web/Controllers/ReservationsController.cs
+++ ITCAir.Web/Controllers/ReservationsController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ITCAir.Web.Attributes;
+using ITCAir.Web.Models.Reservations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ITCAir.Web.Controllers
 {
     public class ReservationsController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ReservationDate]
+        public IActionResult ProcessFirstStepReservation(FirstStepReservationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("ReservationsFlights");
+            }
+            return View("Index");
         }
     }
 }
---
+++ ITCAir.Web/Models/Reservations/FirstStepReservationModel.cs
@@ -0,0 +1,24 @@
+﻿using ITCAir.Web.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITCAir.Web.Models.Reservations
+{
+    public class FirstStepReservationModel
+    {
+        [Required]
+        public string From { get; set; }
+        [Required]
+        public string To { get; set; }
+        [Required]
+        [ReservationDate]
+        public DateTime Date { get; set; }
+        public bool OneWay { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Negative values or 0 are not accepted")]
+        public int People { get; set; }
+    }
+}

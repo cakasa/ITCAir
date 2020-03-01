ca81b795a074f280e759346c65cf798c94f11dfd
cakas <cakas@abv.bg>
2020-03-01 06:47:25 +0200

    Added Logo

--- ITCAir.Data/Enum/FlightFilterType.cs
+++ ITCAir.Data/Enum/FlightFilterType.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ITCAir.Data.Enum
 {
     public enum FlightFilterType
     {
+        [Display(Name = "No Filter")]
         None,
         From,
         To
--- ITCAir.Data/Enum/ReservationFilterType.cs
+++ ITCAir.Data/Enum/ReservationFilterType.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ITCAir.Data.Enum
 {
     public enum ReservationFilterType
     {
+        [Display(Name = "No Filter")]
         None,
         Email
     }
